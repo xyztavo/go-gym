60920d842f2a52ecaefc603b466f377bb6477093
gustafer <gustavobreasy@gmail.com>
2024-09-30 21:47:33 -0300

    feat: add exercises, routines and exercises routines table

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Todos
-- [ ] create exercises table, must have type (reps or seconds) id, name, description, gif, amount
-- [ ] create routines table that have exercises id, name, description, image
+- [x] create exercises table, must have type (reps or seconds) id, name, description, gif, amount
+- [x] create routines table that have exercises id, name, description, image
+- [ ] create admin user that can create exercises and routines 
--- internal/database/database.go
+++ internal/database/database.go
@@ -29,6 +29,26 @@ func Migrate() error {
 	email VARCHAR(40) UNIQUE NOT NULL, 
 	password VARCHAR(200) NOT NULL
 	);
+	CREATE TABLE IF NOT EXISTS exercises (
+	id VARCHAR(40) PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	description VARCHAR(40) NOT NULL,
+	gif VARCHAR(40) NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS routines (
+	id VARCHAR(40) PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	description VARCHAR(40) NOT NULL,
+	thumb VARCHAR(40) NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS exercises_routines (
+    id VARCHAR(40) PRIMARY KEY,
+    routine_id VARCHAR(40) NOT NULL,
+    exercise_id VARCHAR(40) NOT NULL,
+    reps INT NOT NULL,
+    FOREIGN KEY (routine_id) REFERENCES routines(id),
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id)
+	);
 	`)
 	if err != nil {
 		return err
