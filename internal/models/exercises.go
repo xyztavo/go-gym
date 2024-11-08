af083592f5f6d2d69a58e0b0c97fe54e56fbb8de
gustafer <gustavobreasy@gmail.com>
2024-11-08 12:47:28 -0300

    fix: id is not required when creating exercis

--- internal/models/exercises.go
+++ internal/models/exercises.go
@@ -7,7 +7,6 @@ type Exercise struct {
 	Gif         string `json:"gif"`
 }
 type CreateExercise struct {
-	Id          string `json:"id" validate:"required"`
 	Name        string `json:"name" validate:"required"`
 	Description string `json:"description" validate:"required"`
 	Gif         string `json:"gif" validate:"required"`
